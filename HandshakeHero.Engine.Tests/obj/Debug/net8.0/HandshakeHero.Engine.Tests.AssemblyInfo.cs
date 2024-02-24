606e33dd1ffef72febb4ebe82602354dd7d2c22d
Lee-Roy Ashworth <leeroya@gmail.com>
2024-02-24 21:28:54 +0200

    chore: Some test config changes

--- HandshakeHero.Engine.Tests/obj/Debug/net8.0/HandshakeHero.Engine.Tests.AssemblyInfo.cs
+++ HandshakeHero.Engine.Tests/obj/Debug/net8.0/HandshakeHero.Engine.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HandshakeHero.Engine.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+351b335872ef5e9eec2cf16f36816ba602beb534")]
 [assembly: System.Reflection.AssemblyProductAttribute("HandshakeHero.Engine.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HandshakeHero.Engine.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
